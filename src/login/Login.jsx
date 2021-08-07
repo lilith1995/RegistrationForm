37a6b1f3516aac437424911825735d44853074d9
lilith1995 <lilith.mnatsakanian@gmail.com>
2021-08-07 19:01:35 +0400

    Fixed problems

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,58 +1,76 @@
 import "./Login.css";
-import { useState } from "react";
+import { useState, useHistory } from "react";
 
 export const Login = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
+  const [mainClass, setMainClass] = useState("bounceRight");
   const [signupEmail, setSignupEmail] = useState("");
   const [signupPassword, setSignupPassword] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [refreshToken, setRefreshToken] = useState("");
 
+  // const history = useHistory();
+  // history.push('\home')
   const Signin = () => {
     console.log(loginEmail, loginPassword);
+    if (!isValidEmail(loginEmail)) {
+      alert("invalid email");
+    } else {
+      fetch("http://localhost:4000/users/login", {
+        method: "POST",
+        body: JSON.stringify({ email: loginEmail, password: loginPassword }),
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          // history.push('\home')  //after pushing data to localstorage or redux
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
   };
-
-  fetch("http://localhost:3000/users", {
-    method: "POST",
-    body: JSON.stringify({ email: loginEmail, password: loginPassword }),
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  }).then(res => {
-      return res.json()
-    }).then((res) => {
-      if (res.accessToken && res.refreshToken) {
-        setRefreshToken(res.refreshToken);
-        setAccessToken(res.accessToken);
-      } else {
-        alert(res.message);
-      }
-      console.log(res)})
-    .catch(e => {
-      console.log(e);
-    });
-
+  const isValidEmail = (email) => {
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  };
   const Signup = () => {
     console.log(signupEmail, signupPassword);
+    if (!isValidEmail(signupEmail)) {
+      alert("invalid email");
+    } else {
+      fetch("http://localhost:4000/users", {
+        method: "POST",
+        body: JSON.stringify({ email: signupEmail, password: signupPassword }),
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          if (res.accessToken && res.refreshToken) {
+            setRefreshToken(res.refreshToken);
+            setAccessToken(res.accessToken);
+          } else {
+            alert(res.message);
+          }
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
   };
-
-  fetch("http://localhost:4000/users/login", {
-    method: "POST",
-    body: JSON.stringify({ email: signupEmail, password: signupPassword }),
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  }).then(res => {
-    return res.json()
-  }).then(res => console.log(res))
-    .catch(e => {
-      console.log(e);
-    });
-
 
   return (
     <section className="user">
@@ -64,7 +82,10 @@ export const Login = () => {
               With your personal account, you can track your favorite properties
               and receive custom alerts about new listings that come to market.
             </p>
-            <button className="user_unregistered-signup" id="signup-button">
+            <button
+              className="user_unregistered-signup"
+              id="signup-button"
+              onClick={() => setMainClass("bounceLeft")}>
               Join us!
             </button>
           </div>
@@ -74,12 +95,17 @@ export const Login = () => {
               Don’t miss out on the right home for you — browse up-to-date
               listings, refine your search and more.
             </p>
-            <button className="user_registered-login" id="login-button">
+            <button
+              className="user_registered-login"
+              id="login-button"
+              onClick={() => setMainClass("bounceRight")}>
               Login
             </button>
           </div>
         </div>
-        <div className="user_options-forms" id="user_options-forms">
+        <div
+          className={`user_options-forms ${mainClass}`}
+          id="user_options-forms">
           <div className="user_forms-login">
             <h2 className="forms_title">Welcome</h2>
             <form className="forms_form">
@@ -110,7 +136,7 @@ export const Login = () => {
                   Forgot password?
                 </button>
                 <input
-                  type="submit"
+                  type="button"
                   defaultValue="Log In"
                   className="forms_buttons-action"
                   onClick={Signin}
@@ -153,7 +179,7 @@ export const Login = () => {
               </fieldset>
               <div className="forms_buttons">
                 <input
-                  type="submit"
+                  type="button"
                   defaultValue="Sign up"
                   className="forms_buttons-action"
                   onClick={Signup}
@@ -162,33 +188,33 @@ export const Login = () => {
             </form>
           </div>
         </div>
-        <div classname="column">{accessToken}</div>
-        <div classname="column">{refreshToken }</div>
+        <div className="column">{accessToken}</div>
+        <div className="column">{refreshToken}</div>
       </div>
     </section>
   );
 };
 
-window.onload = function () {
-  const signupButton = document.getElementById("signup-button"),
-    loginButton = document.getElementById("login-button"),
-    userForms = document.getElementById("user_options-forms");
+// window.onload = function () {
+//   const signupButton = document.getElementById("signup-button"),
+//     loginButton = document.getElementById("login-button"),
+//     userForms = document.getElementById("user_options-forms");
 
-  signupButton.addEventListener(
-    "click",
-    () => {
-      userForms.classList.remove("bounceRight");
-      userForms.classList.add("bounceLeft");
-    },
-    false
-  );
+//   signupButton.addEventListener(
+//     "click",
+//     () => {
+//       userForms.classList.remove("bounceRight");
+//       userForms.classList.add("bounceLeft");
+//     },
+//     false
+//   );
 
-  loginButton.addEventListener(
-    "click",
-    () => {
-      userForms.classList.remove("bounceLeft");
-      userForms.classList.add("bounceRight");
-    },
-    false
-  );
-};
+//   loginButton.addEventListener(
+//     "click",
+//     () => {
+//       userForms.classList.remove("bounceLeft");
+//       userForms.classList.add("bounceRight");
+//     },
+//     false
+//   );
+// };
